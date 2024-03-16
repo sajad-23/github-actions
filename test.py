f0e42ede316ea82b0c626e623176d623a9aacfc0
sajad-23 <am.betdevel@gmail.com>
2024-03-16 23:54:40 +0700

    update in demo.yml

--- test.py
+++ test.py
@@ -1,8 +1,13 @@
-import pytest
+#import pytest
 
 
 
 class TestNumber:
+    
     def test_equality(self):
         x = 'Mohammed'
         assert 'm' in x
+    def test_name(self):
+        name = 'Mohammed'
+        print(name)
+        assert 'Mohammed' == name
